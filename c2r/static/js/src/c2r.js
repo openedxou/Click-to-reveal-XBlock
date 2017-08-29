99a20922cc479bd21176d52165f124f9f2621d83
Matthew Harrington <Mat.Harrington.Work@gmail.com>
2017-08-29 14:42:36 +0100

    Update c2r.js

--- c2r/static/js/src/c2r.js
+++ c2r/static/js/src/c2r.js
@@ -2,23 +2,6 @@
 function C2RBlock(runtime, element, initargs) {
     /* Runtime stuff, nothing here for now */
 }
- /* old version
- function revealComment(e, textLabel, showActionLabel, hideActionLabel) {
-  var comment = $(e.target).parents('.c2r-container').find('.c2r-comment')[0];
-  
-  if (comment.style.display == 'block') {
-   comment.style.display = 'none';
-   comment.setAttribute('aria-hidden', 'true');
-   e.target.textContent = showActionLabel+textLabel;
-   e.target.setAttribute('aria-expanded', 'false');
-  }
-  else {
-   comment.style.display = 'block';
-   comment.removeAttribute('aria-hidden');
-   e.target.textContent = hideActionLabel+textLabel;
-   e.target.setAttribute('aria-expanded', 'true');
-  }
-*/
   function revealDescription(e,showText,hideText) {
   var comment = $(e.target).parents('.c2r-container').find('.c2r-comment')[0];
   

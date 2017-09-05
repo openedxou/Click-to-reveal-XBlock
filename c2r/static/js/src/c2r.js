5a53650cfeaa38b7ff0a8fa3ddaf64e8656cb418
Matthew Harrington <Mat.Harrington.Work@gmail.com>
2017-09-05 10:38:19 +0100

    Attempt at fixing the FA problem
    When the icon is clicked, the <i> tag gets in the way.

--- c2r/static/js/src/c2r.js
+++ c2r/static/js/src/c2r.js
@@ -5,18 +5,19 @@ function C2RBlock(runtime, element, initargs) {
 
 function revealDescription(e, showText, hideText) {
     var comment = $(e.target).parents('.c2r-container').find('.c2r-comment')[0];
+    var button = $(e.target).parents('.c2r-container').find('.c2r-button')[0];
     var showFinal = '<i class="fa fa-eye" aria-hidden="true"></i> ' + showText;
     var hideFinal = '<i class="fa fa-eye" aria-hidden="true"></i> ' + hideText;
     
     if (comment.style.display == 'block') {
         comment.style.display = 'none';
         comment.setAttribute('aria-hidden', 'true');
-        e.target.innerHTML = showFinal;
-        e.target.setAttribute('aria-expanded', 'false');
+        button.innerHTML = showFinal;
+        button.setAttribute('aria-expanded', 'false');
     } else {
         comment.style.display = 'block';
         comment.removeAttribute('aria-hidden');
-        e.target.innerHTML = hideFinal;
-        e.target.setAttribute('aria-expanded', 'true');
+        button.innerHTML = hideFinal;
+        button.setAttribute('aria-expanded', 'true');
     }
 }

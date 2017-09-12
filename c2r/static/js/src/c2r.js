682b50e0f2df173392e7ba7dda09796ef7f0d5c2
Matthew Harrington <Mat.Harrington.Work@gmail.com>
2017-09-12 15:31:27 +0100

    New version
    Newer, JQuerier... better?

--- c2r/static/js/src/c2r.js
+++ c2r/static/js/src/c2r.js
@@ -3,21 +3,19 @@ function C2RBlock(runtime, element, initargs) {
     /* Runtime stuff, nothing here for now */
 }
 
-function revealDescription(e, showText, hideText) {
-    var comment = $(e.target).parents('.c2r-container').find('.c2r-comment')[0];
-    var button = $(e.target).parents('.c2r-container').find('.c2r-button')[0];
+function showDesc(event, showText, hideText) {
     var showFinal = '<i class="fa fa-eye" aria-hidden="true"></i> ' + showText;
     var hideFinal = '<i class="fa fa-eye" aria-hidden="true"></i> ' + hideText;
+    var buttonPressed = $(event.target);
+    var comment = buttonPressed.nextAll('.c2r-comment');
     
-    if (comment.style.display == 'block') {
-        comment.style.display = 'none';
-        comment.setAttribute('aria-hidden', 'true');
-        button.innerHTML = showFinal;
-        button.setAttribute('aria-expanded', 'false');
-    } else {
-        comment.style.display = 'block';
-        comment.removeAttribute('aria-hidden');
-        button.innerHTML = hideFinal;
-        button.setAttribute('aria-expanded', 'true');
-    }
+    comment.slideToggle(200, function() {
+        if (comment.is(":hidden")) {
+            buttonPressed.text(showFinal);
+            buttonPressed.attr("aria-expanded", "false");
+        } else {
+            buttonPressed.text(hideFinal);
+            buttonPressed.attr("aria-expanded", "true");
+        }
+    });
 }
